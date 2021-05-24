1871d6469f608ed08bbeaca0a8b6b49f4a7b1ebb
Eric <eric.s.russell@wsu.edu>
2021-05-24 14:00:47 -0500

    Update GACP_Epro_QC.py
    Update filenames and turn on the Reddyproc output calls

--- GACP_Epro_QC.py
+++ GACP_Epro_QC.py
@@ -21,16 +21,17 @@ import os
 import datetime
 import warnings
 # Change this path to the directory where the LTAR_Flux_QC.py file is located
-os.chdir(r'C:\*\GitHub\Flux_Processing_Development\GACP')       
+os.chdir(r'C:\Users\russe\Documents\GitHub\GACP_Flux_Processing')       
 import GACP_Epro_QC_Functions as LLT
 import AF_Rename as AFN
-# ds = '2017-01-01'
-# de = '2020-01-01'
+import Reddy_Format as REF
 
-files = glob.glob(r'C:\Users\russe\Desktop\Projects\Active\GACP\Agg\*.csv')
+#Change this
+files = glob.glob(r'C:\Users\russe\Documents\GitHub\GACP_Flux_Processing\Data\Agg\*.csv')
+
 # File with upper and lower limits for the flux values for each site based on visual inspection of each dataset
-info = pd.read_csv(r'C:\*\GitHub\Flux_Processing_Development\GACP\Bound_Limits_EPro.csv', index_col = 'Var', header = 0)
-AF_Cols=r'C:\*\GitHub\Flux_Processing_Development\GACP\AF_EP_EF_Column_Renames.csv'
+info = pd.read_csv(r'C:\Users\russe\Documents\GitHub\GACP_Flux_Processing\Bound_Limits_EPro.csv', index_col = 'Var', header = 0)
+AF_Cols=r'C:\Users\russe\Documents\GitHub\GACP_Flux_Processing\AF_EP_EF_Column_Renames.csv'
 
 #%%files
 for K in range (0,len(files)):
@@ -43,12 +44,15 @@ for K in range (0,len(files)):
     # df = df.astype(float)
     df = LLT.indx_fill(df,'30min') # Fill in and missing half-hours in the dataset to have a continuous data set from start time to end.
     #%%
+    
     data_qc, data_flags = LLT.Grade_cs(df, info) #QC flux data; new data to use is the data_qc dataframe; will be the one AF_Rename cues from  
     # Works to here for the moment; not sure what the hell is going on.
     qn = (data_qc['air_temperature']>150)&(data_qc['air_temperature']<400) #Check to seee if temperature is in Kelvin or not
     data_qc['air_temperature'][qn] = data_qc['air_temperature'][qn] - 273.15 # Convert temperature to Celsius
     
-    data_met = LLT.Met_QAQC(Tair=data_qc['air_temperature'], RH=data_qc['RH'], VPD=data_qc['VPD']/100, e=data_qc['e']/100, es=data_qc['es']/100, WS=data_qc['wind_speed'], WD=data_qc['wind_dir'], P = data_qc['air_pressure']/1000)
+    data_met = LLT.Met_QAQC(Tair=data_qc['air_temperature'], RH=data_qc['RH'].astype(float), VPD=data_qc['VPD'].astype(float), e=data_qc['e']/100, es=data_qc['es']/100, WS=data_qc['wind_speed'], 
+                            WD=data_qc['wind_dir'], P = data_qc['air_pressure']/1000, SW_In = data_qc['SW_in_Avg'], SW_Out = data_qc['SW_out_Avg'], LW_In = data_qc['LW_in_Avg'],
+                            LW_Out = data_qc['LW_out_Avg'], Rn = data_qc['Rn'], PAR=data_qc['PAR_density_Avg'])
     
     Data_O = pd.concat([data_qc, data_flags,data_met],axis = 1, sort=False)
     
@@ -65,38 +69,13 @@ for K in range (0,len(files)):
     Data_O['co2_flux'] = Data_O['co2_flux'][FC['FC']] 
     
     AF_O = AFN.AmeriFlux_Rename(Data_O, info['Val_L']['outfile'], 'EPRO', AF_Cols, 'CSV', GRADE=False)
-    Data_O.to_csv('C:\\*\\' + files[K][48:] + '_Processed.csv', index=False)
+    Data_O.to_csv('C:\\Users\\russe\\Documents\\GitHub\\GACP_Flux_Processing\\'+ files[K][62:-4] + '_Processed_Test.csv', index=False)
     
     
     # ReddyProc based data is missing either PAR or incoming shortwave radiation to be able to gapfill; otherwise should be easy enough to make happen with the current data set.
-    # FileName = 'E:\\WorkPC_Desktop\\LTAR\\LTAR_National_Projects\\REDDY\\L2_QC_Gapped_'+nme+'.txt'
-    # REF.REddy_Format(data_qc, FileName,'Start_AF') # Function that format
+    FileName = 'C:\\Users\\russe\\Desktop\\Projects\\Active\\GACP\\QC_Gapped_'+files[K][62:67]+'REddy_format.txt'
+    REF.REddy_Format(AF_O, FileName,'Start_AF') # Function that format
     
     
     df = [];data_qc = []
     
-
-'''
-Code Graveyard; not useless but not needed at the moment; could be brought back depending on needs but only acts as clutter for current usage
-
-
-    if ('/' in str(df['TIMESTAMP_START'][0])) | ('-' in str(df['TIMESTAMP_START'][0])):
-        df.index = pd.to_datetime(df['TIMESTAMP_START'])
-    else: # Convert timestamp to time-index for easier use
-        for k in range (0,len(df)):
-            Y =  str(int(df['TIMESTAMP_START'][k]))[0:4]
-            M =  str(int(df['TIMESTAMP_START'][k]))[4:6]  
-            D =  str(int(df['TIMESTAMP_START'][k]))[6:8]
-            hh = str(int(df['TIMESTAMP_START'][k]))[8:10] 
-            mm = str(int(df['TIMESTAMP_START'][k]))[10:12]
-            dt.append(Y+'-'+M+'-'+D+' '+hh+':'+mm)
-        dt = pd.DataFrame(dt);df.index = dt[0]
-        df.index=pd.to_datetime(df.index) # Time-based index
-        if 'TimeStamp' in df.columns: df=df.drop(columns = 'TimeStamp') # Specific to JORN
-        if 'timestamp' in df.columns: df=df.drop(columns = 'timestamp') # Specific to JORN
-        if 'TIMESTAMP' in df.columns: df=df.drop(columns = 'TIMESTAMP') # Specific to JORN
-        if 'HRMIN' in df.columns: df=df.drop(columns = 'HRMIN') # Specific to UCB because formating time is bad
-        
-        
-        
-'''
